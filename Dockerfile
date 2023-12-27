0d4acb69b7b66acf672e2a67d75299fad6e4e007
aryan619348 <aryan619348@gmail.com>
2023-12-28 02:49:51 +0530

    added docker compose

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,8 @@ FROM python:3.10-alpine3.18
 
 COPY . /app
 WORKDIR /app
+
+
 RUN pip install -r requirements.txt
+
 CMD python app.py
--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from redis import Redis
 
 app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    # Increment a counter in Redis on each visit
+    redis.incr('visit_count')
+
+    # Retrieve the visit count from Redis
+    visit_count = redis.get('visit_count').decode('utf-8')
+
+    return f'Hello, World! - Visit Count: {visit_count}\n'
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=8000)
+    app.run(debug=True, host='0.0.0.0',port=8000)
