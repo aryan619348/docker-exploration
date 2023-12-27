4bce92be8d598678bc9288d28356f35cd3f27466
aryan619348 <aryan619348@gmail.com>
2023-12-28 04:03:10 +0530

    updated the message

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello_world():
     # Retrieve the visit count from Redis
     visit_count = redis.get('visit_count').decode('utf-8')
 
-    return f'Hello, World! - Visit Count: {visit_count}\n'
+    return f'Hello, Welcome to My Docker experiment! \n- Visit Count: {visit_count}\n'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=8000)
